abed9e3c6d442b0cdb39bfb1111eb58ebcf0d968
tugaskelompok0 <jjgroupg@gmail.com>
2025-02-19 21:14:38 +0700

    first commit

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function toggleMenu() {
     var menu = document.getElementById("navMenu");
     menu.classList.toggle("active");
 
-    // Toggle tombol close dan hamburger
     var closeBtn = document.querySelector(".close-menu");
     if (menu.classList.contains("active")) {
         closeBtn.style.display = "block";
